0b71373dcaa495e0b75197c60b0037510ed5bac0
MitheshGS <gsmithesh1004@gmail.com>
2025-10-07 18:46:25 +0530

    Update StockSegmentTree.java

--- StockSegmentTree.java
+++ StockSegmentTree.java
@@ -1,1 +1,4 @@
+//A Segment Tree is a versatile and efficient tree-based data structure used to store information about intervals or segments.
+//Its primary application is to allow for fast querying (like finding the sum, minimum, or maximum) over a specific range within an array, and fast updating of elements in the array. 
+//It's particularly useful when both range queries and single-point updates are frequent operations.
 
